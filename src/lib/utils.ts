b66408263b1e83a28d6eb2026eb4bf720465943b
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2024-10-25 09:15:14 +0300

    Created members workspace

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,16 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function generateInviteCode(length: number) {
+  let result = "";
+
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < length; i += 1) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+
+  return result;
+}
